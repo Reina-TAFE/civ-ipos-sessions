0457e47c6986c3b071bb248ce58817449f1efeac
Reina <20066312@tafe.wa.edu.au>
2025-08-06 15:33:45 +0800

    Chore(week 3) activity 2

--- week3/in-class-demos/workshop_part2/activity_2/data_reader.py
+++ week3/in-class-demos/workshop_part2/activity_2/data_reader.py
@@ -1,27 +1,43 @@
 # Remember you file paths when you attempt use this file reader
 # import the reuseable csv library - docs at: https://docs.python.org/3/library/csv.html
+import csv
+
 
 # Function to read data from CSV file
 def read_sales_data(filename):
-# Task 1: Read data from CSV file
-    pass
+    # Task 1: Read data from CSV file
+    sales_data = []
+    with open("sales_data_v2.csv", newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            row["Product Name"] = row["Product"]
+            row["Units Sold"] = int(row["Units Sold"])
+            row["Unit Price"] = float(row["Unit Price"])
+            row["Total Revenue"] = float(row["Total Revenue"])
+            sales_data.append(row)
+    return sales_data
+
 
 # Task 2: Calculate total revenue for each product
+sales_data = read_sales_data("sales_data_v2.csv")
+for product in sales_data:
+    print(product["Product Name"])
+    print(product["Total Revenue"])
+
 
 # Task 3: Identify the product with the highest total units sold
 
 # Task 4: Calculate average unit price for each product - watch out for division by zero
 
 
-
 # Display results
-print("Total revenue for each product:")
-for product, revenue in product_revenue.items():
-    print(f"{product}: ${revenue:.2f}")
-
-print("\nThe product with the highest total units sold:")
-print(max_units_sold_product)
-
-print("\nAverage unit price for each product:")
-for product, avg_price in product_unit_price.items():
-    print(f"{product}: ${avg_price:.2f}")
+# print("Total revenue for each product:")
+# for product, revenue in product_revenue.items():
+#     print(f"{product}: ${revenue:.2f}")
+#
+# print("\nThe product with the highest total units sold:")
+# print(max_units_sold_product)
+#
+# print("\nAverage unit price for each product:")
+# for product, avg_price in product_unit_price.items():
+#     print(f"{product}: ${avg_price:.2f}")
