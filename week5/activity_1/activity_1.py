f760e6552b6fa15a0a0a2bbb8bcb92c959a49a27
Reina-TAFE <20066312@tafe.wa.edu.au>
2025-08-20 16:22:44 +0800

    Feat(week5) Complete week5 activities

--- week5/activity_1/activity_1.py
+++ week5/activity_1/activity_1.py
@@ -1,41 +1,52 @@
 def main():
     # Define 3 helper functions for text-to-bytes conversion, bytes-to-text conversion, and byte reversal
     def reverse_bytes(bytes_data):
-        pass
+        return bytes_data[:: -1]
 
     def convert_bytes_to_text(bytes_data):
-        pass
+        return bytes_data.decode('utf-8').strip()
 
     def convert_text_to_bytes(bytes_data):
-        pass
+        return bytes_data.encode().hex()
 
     # Main program logic
     # Open the binary file for reading and create output text and bytes files for writing using the context manager
-
+    try:
+        with open('./data.bin', 'rb') as bin_file, \
+                open("./converted.txt", 'w') as converted_text_file, \
+                open("./reversed_bytes.txt", 'wb') as reversed_bytes_file:
+            bin_data = bin_file.readlines()
             # Iterate through each line in the binary file
-
+            for line in bin_data:
                 # Decode the line to Unicode string and remove leading/trailing whitespaces
-    
+                unicode_line = convert_bytes_to_text(line)
 
                 # Check if the line starts with "TEXT:"
-          
+                if unicode_line.startswith('TEXT:'):
+                    text = unicode_line[len('TEXT:'):].upper() + "\n"
+                    converted_text_file.write(text)
 
                 # Check if the line starts with "BYTES:"
-      
+                elif unicode_line.startswith('BYTES:'):
                     # Extract the string and encode to hexadecimal
-          
-                    # Extract byte content, convert to bytes object(using fromhex()), 
-              
+                    line_string = convert_text_to_bytes(unicode_line[:len('BYTES:')])
+                    # Extract byte content, convert to bytes object(using fromhex()),
+                    line_bytes = convert_text_to_bytes(unicode_line[len('BYTES:'):])
+                    line_bytes_object = bytes.fromhex(line_bytes)
+                    # converted_text_file.write(line_bytes_object)
                     # reverse bytes, and write to bytes file
-   
+                    reverse_line_bytes = reverse_bytes(line_bytes_object)
+                    reversed_bytes_file.write(reverse_line_bytes)
+            print("complete")
 
-        # Print success message
-  
-
+    except IOError as binary_error_msg:
+            # Print success message
+            print(binary_error_msg)
     # Handle file I/O errors
     # IOError - see definition, also Documentation: https://docs.python.org/3/library/io.html#
 
     # Handle other exceptions using the exception class
+
 
 if __name__ == "__main__":
     main()
--- week5/activity_2/activity_2.py
+++ week5/activity_2/activity_2.py
@@ -1,36 +1,38 @@
 def main():
-    pass
-        # Phase 1: File Navigation
+    # Phase 1: File Navigation
+    with open("./data_v2.bin", 'rb') as file:
+        data = file.read()
+        # Navigate to the 5th byte position
+        file.seek(4)
+        # Read and print the next 4 bytes from the current position
+        print(file.read(4).decode('utf-8'))
 
-            # Navigate to the 5th byte position
+        # Move the file pointer to the beginning of the file
+        file.seek(0)
+        # Read and print the first 8 bytes from the file
+        print(file.read(8))
 
-            # Read and print the next 4 bytes from the current position
+        # Print the current file pointer position
+        print(file.tell())
 
-
-            # Move the file pointer to the beginning of the file
-
-            # Read and print the first 8 bytes from the file
-
-
-            # Print the current file pointer position
-
-
-            # Use the find() method to search for the string "ABC" in the file
-
-
-
+        # Use the find() method to search for the string "ABC" in the file
+        print(data.find("ABC".encode()))
 
 
-        # Re-open the data.bin file in binary write-append mode
-
-            # Use the tell() method to get the current file pointer position and store it as a bookmark
 
 
-            # Write the string "XYZ" to the file
+    # Re-open the data.bin file in binary write-append mode
+    with open("./data_v2.bin", "ab") as file:
+        data = file.read()
+        # Use the tell() method to get the current file pointer position and store it as a bookmark
+        pointer = file.tell()
 
+        # Write the string "XYZ" to the file
+        file.write("XYZ".encode())
 
-            # Use the bookmarked pointer position to append the string "123" to the file
-
+        # Use the bookmarked pointer position to append the string "123" to the file
+        file.seek(pointer)
+        file.write("123".encode())
     # create three non naked exception handlers for: 
     # not finding the file, i/o error & all other exceptions
 
