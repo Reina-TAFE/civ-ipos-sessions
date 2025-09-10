ea465d607c8de1706743b4c00aff19113bc1947e
Reina-TAFE <20066312@tafe.wa.edu.au>
2025-09-10 16:24:31 +0800

    Feat(sphinx) conf.py

--- week7/sphinx_activity/docs/conf.py
+++ week7/sphinx_activity/docs/conf.py
@@ -21,7 +21,8 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
